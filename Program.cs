3157b770c38bbbe2118bc0d6f0cea6292dc3bbae
Gastonloco <ferreyragaston351@gmail.com>
2022-04-23 15:55:48 -0300

    Calculo fecha

---
+++ Program.cs
@@ -0,0 +1,252 @@
+﻿using System;
+
+namespace fecha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor=ConsoleColor.DarkBlue; //definimos el color de fondo
+            Console.ForegroundColor=ConsoleColor.White;  //definimos el text
+            Console.Clear();
+
+            int dia=0,mes,año;
+            double cal1, cal2,cal3;
+            string meschar="";
+            int mesdia=0;
+            string imp;
+
+            do{
+            Console.Write("\n Ingresar el numero del mes= ");
+            mes= Convert.ToInt32(Console.ReadLine());
+
+            }
+            while ( mes<1 || mes>12 );
+               
+            Console.Write("\n Ingresar el numero del año= ");
+            año= Convert.ToInt32(Console.ReadLine());
+
+            cal1=año%4; //porcentaje para calcular el resto
+            cal2=año%100;
+            cal3=año%400;
+
+
+if (año>=1900 && año<=2022) 
+{
+    if (cal1==0 && cal2 != 0 || cal3==0)
+    {      
+        switch(mes)
+        {
+         case 1 :
+          mesdia=31;
+         break;
+         
+         case 2 :
+          mesdia=29;
+         break;
+
+         
+         case 3 :
+          mesdia=31;
+         break;
+         
+         case 4 :
+          mesdia=30;
+         break;
+         
+         case 5 :
+          mesdia=31;
+         break;
+         
+         case 6 :
+          mesdia=30;
+         break;
+         
+         case 7 :
+          mesdia=31;
+         break;
+         
+         case 8 :
+          mesdia=31;
+         break;
+
+         case 9 :
+          mesdia=30;
+         break;
+         
+         case 10 :
+          mesdia=31;
+         break;
+         
+         case 11 :
+          mesdia=30;
+         break;
+         
+         case 12 :
+          mesdia=31;
+         break;
+        } 
+     
+      do{
+            Console.Write("\n Ingresar el numero del día= ");
+            dia= Convert.ToInt32(Console.ReadLine());
+
+            }
+            while ( dia > mesdia || dia<1); 
+    
+    if(dia==mesdia)
+    {
+        dia=1;
+        if(mes==12)
+        {
+            mes=1;
+            año=año+1;
+        }
+
+    }
+    else{
+        dia=dia+1;
+    }
+    }
+
+else
+     { 
+    switch(mes)
+        {
+         case 1 :
+          mesdia=31;
+         break;
+         
+         case 2 :
+          mesdia=28;
+         break;
+    
+         case 3 :
+          mesdia=31;
+         break;
+         
+         case 4 :
+          mesdia=30;
+         break;
+         
+         case 5 :
+          mesdia=31;
+         break;
+         
+         case 6 :
+          mesdia=30;
+         break;
+         
+         case 7 :
+          mesdia=31;
+         break;
+         
+         case 8 :
+          mesdia=31;
+         break;
+
+         case 9 :
+          mesdia=30;
+         break;
+         
+         case 10 :
+          mesdia=31;
+         break;
+         
+         case 11 :
+          mesdia=30;
+         break;
+         
+         case 12 :
+          mesdia=31;
+         break;
+        }
+
+              do
+              {
+
+            
+            Console.Write("\n Ingresar el numero del día= ");
+            dia= Convert.ToInt32(Console.ReadLine());
+            
+              }
+            while ( dia > mesdia || dia<1); 
+
+    if(dia==mesdia)
+    {
+        dia=1;
+        if(mes==12)
+        {
+            mes=1;
+            año=año+1;
+        }
+
+    }
+    else{
+        dia=dia+1;
+    }
+} 
+     switch(mes)
+        {
+         case 1 :
+          meschar="Enero";
+         break;
+         
+         case 2 :
+          meschar="Febrero";
+         break;
+    
+         case 3 :
+          meschar="Marzo";
+         break;
+         
+         case 4 :
+          meschar="Abril";
+         break;
+         
+         case 5 :
+          meschar="Mayo";
+         break;
+         
+         case 6 :
+          meschar="Junio";
+         break;
+         
+         case 7 :
+          meschar="Julio";
+         break;
+         
+         case 8 :
+          meschar="Agosto";
+         break;
+
+         case 9 :
+          meschar="Septiembre";
+         break;
+         
+         case 10 :
+          meschar="Octubre";
+         break;
+         
+         case 11 :
+          meschar="Noviembre";
+         break;
+         
+         case 12 :
+          meschar="Diciembre";
+         break;
+        }
+        Console.WriteLine("\n*************************************************"); 
+        Console.WriteLine("\n"); 
+        imp=  "El dia siguiente es  "+dia+" de "+ meschar+", del año "+año;       
+        Console.WriteLine("\n"+imp);  
+        Console.WriteLine("\n"); 
+        Console.WriteLine("\n*************************************************");         
+        Console.WriteLine("\n"); 
+        Console.WriteLine("\n"); 
+}
+
+}
+}
+}
+
